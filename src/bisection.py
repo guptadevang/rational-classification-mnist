74702f9bb313ac16f7c7454f2daa903eae841b72
guptadevang <guptadevang793@gmail.com>
2024-07-09 00:00:34 +0200

    bisection update

--- src/bisection.py
+++ src/bisection.py
@@ -47,16 +47,16 @@ def generate_rational_function(alpha_num, alpha_den, n):
     numerator = 0
     for idx in p_indices:
         term = 1
-        for i, exp in enumerate(idx):
-            term *= variables[i]**exp
+        for I, exp in enumerate(idx):
+            term *= variables[I]**exp
         idx_str = ''.join(map(str, idx))
         numerator += sp.symbols(f'p{idx_str}') * term
 
     denominator = 0
     for idx in q_indices:
         term = 1
-        for i, exp in enumerate(idx):
-            term *= variables[i]**exp
+        for I, exp in enumerate(idx):
+            term *= variables[I]**exp
         idx_str = ''.join(map(str, idx))
         denominator += sp.symbols(f'q{idx_str}') * term
 
@@ -78,8 +78,8 @@ def difference_function(x, params):
     p = params[:len(p_indices)]
     q = params[len(p_indices):]
     
-    num = sum([p[i] * (x1**idx[0]) * (x2**idx[1]) for i, idx in enumerate(p_indices)])
-    den = sum([q[i] * (x1**idx[0]) * (x2**idx[1]) for i, idx in enumerate(q_indices)])
+    num = sum([p[I] * (x1**idx[0]) * (x2**idx[1]) for I, idx in enumerate(p_indices)])
+    den = sum([q[I] * (x1**idx[0]) * (x2**idx[1]) for I, idx in enumerate(q_indices)])
     
     rational_val = num / den
     actual_val = f((x1, x2))
@@ -90,8 +90,8 @@ def difference_function(x, params):
 
 def total_difference(params, x_points, y_points):
     total_diff = 0
-    for i in range(x_points.shape[1]):
-        _, diff = difference_function((x_points[0, i], x_points[1, i]), params)
+    for I in range(x_points.shape[1]):
+        _, diff = difference_function((x_points[0, I], x_points[1, I]), params)
         total_diff += diff
     
     return total_diff
@@ -109,20 +109,20 @@ def coordinate_bisection(params, x_points, y_points, tol=1e-15, max_iter=10000, 
     previous_total_diff = total_difference(params, x_points, y_points)
     errors_bisection = [previous_total_diff]
     for iteration in range(max_iter):
-        for i in range(n_params):
+        for I in range(n_params):
             l, u = 0, 1
             while (u - l) / 2 > tol:
                 midpoint = (l + u) / 2
                 params_left = params.copy()
-                params_left[i] = midpoint - tol
+                params_left[I] = midpoint - tol
                 params_right = params.copy()
-                params_right[i] = midpoint + tol
+                params_right[I] = midpoint + tol
 
                 if total_difference(params_left, x_points, y_points) < total_difference(params_right, x_points, y_points):
                     u = midpoint
                 else:
                     l = midpoint
-            params[i] = (l + u) / 2
+            params[I] = (l + u) / 2
         
         current_total_diff = total_difference(params, x_points, y_points)
         errors_bisection.append(current_total_diff)
@@ -177,19 +177,19 @@ def optimized_rational_function(x, params):
     p = params[:len(p_indices)]
     q = params[len(p_indices):]
     
-    num = sum([p[i] * (x1**idx[0]) * (x2**idx[1]) for i, idx in enumerate(p_indices)])
-    den = sum([q[i] * (x1**idx[0]) * (x2**idx[1]) for i, idx in enumerate(q_indices)])
+    num = sum([p[I] * (x1**idx[0]) * (x2**idx[1]) for I, idx in enumerate(p_indices)])
+    den = sum([q[I] * (x1**idx[0]) * (x2**idx[1]) for I, idx in enumerate(q_indices)])
     
     rational_val1 = num / den
     
     return rational_val1
 
 
-initial_y_points = np.array([optimized_rational_function((x1_points[i], x2_points[i]), initial_params) for i in range(len(x1_points))])
+initial_y_points = np.array([optimized_rational_function((x1_points[I], x2_points[I]), initial_params) for I in range(len(x1_points))])
 
 # Optimized y points
-optimized_y_points_bisection = np.array([optimized_rational_function((x1_points[i], x2_points[i]), optimized_params_bisection) for i in range(len(x1_points))])
-optimized_y_points_bfgs = np.array([optimized_rational_function((x1_points[i], x2_points[i]), optimized_params_bfgs) for i in range(len(x1_points))])
+optimized_y_points_bisection = np.array([optimized_rational_function((x1_points[I], x2_points[I]), optimized_params_bisection) for I in range(len(x1_points))])
+optimized_y_points_bfgs = np.array([optimized_rational_function((x1_points[I], x2_points[I]), optimized_params_bfgs) for I in range(len(x1_points))])
 
 sse_bisection = np.sum((optimized_y_points_bisection - y_points)**2)
 sse_bfgs = np.sum((optimized_y_points_bfgs - y_points)**2)
@@ -278,29 +278,31 @@ df_bfgs.to_csv("optimized_differences_bfgs.csv", index=False)
 print(df_bfgs.to_csv(index=False))
 
 
-fig = plt.figure(figsize=(6 , 3))
-ax = fig.add_subplot(121, projection='3d')
+# Plot for Bisection method
+fig = plt.figure(figsize=(6, 6))
+ax = fig.add_subplot(111, projection='3d')
 sc_bisection = ax.scatter(x1_points, x2_points, optimized_y_points_bisection, c=optimized_y_points_bisection, cmap='plasma', marker='o')
 ax.set_xlabel('x1')
 ax.set_ylabel('x2')
 ax.set_zlabel('Optimized y points of function f(x)')
-ax.set_title('pgfs/Optimized f(x) values using bisection method')
+ax.set_title(r'Optimized f(x) values using bisection method')
 fig.colorbar(sc_bisection, ax=ax, format='%.2e')
 fig.savefig('pgfs/optimized_y_points_bisection.pgf', bbox_inches='tight')
 plt.show()
 
-fig = plt.figure(figsize=(6 , 3))
-ax = fig.add_subplot(122, projection='3d')
+# Plot for BFGS method
+fig = plt.figure(figsize=(6, 6))
+ax = fig.add_subplot(111, projection='3d')
 sc_bfgs = ax.scatter(x1_points, x2_points, optimized_y_points_bfgs, c=optimized_y_points_bfgs, cmap='cool', marker='o')
 ax.set_xlabel('x1')
 ax.set_ylabel('x2')
 ax.set_zlabel('Optimized y points of function f(x)')
-ax.set_title('Optimized f(x) values using BFGS method')
+ax.set_title(r'Optimized f(x) values using BFGS method')
 fig.colorbar(sc_bfgs, ax=ax, format='%.2e')
 fig.savefig('pgfs/optimized_y_points_bfgs.pgf', bbox_inches='tight')
 plt.show()
 
-plt.figure(figsize=(6, 3.6))
+plt.figure(figsize=(6, 3))
 plt.plot(errors_bisection, label='Bisection Method')
 plt.plot(errors_bfgs, label='BFGS Method')
 plt.xlabel('Number of iterations')
@@ -312,7 +314,7 @@ plt.grid(True)
 plt.savefig('pgfs/final_difference_iterations.pgf', bbox_inches='tight')
 plt.show()
 
-plt.figure(figsize=(6, 3.6))
+plt.figure(figsize=(6, 3))
 plt.plot(y_points, np.abs(optimized_y_points_bisection - y_points), 'o', label='Bisection method')
 plt.xlabel('y value [f(x)]')
 plt.ylabel('Error difference of f(x)')
@@ -322,7 +324,7 @@ plt.grid(True)
 plt.savefig('pgfs/error_difference_bisection.pgf', bbox_inches='tight')
 plt.show()
 
-plt.figure(figsize=(6, 3.6))
+plt.figure(figsize=(6, 3))
 plt.plot(y_points, np.abs(optimized_y_points_bfgs - y_points), 's', label='BFGS Method')
 plt.xlabel('y value [f(x)]')
 plt.ylabel('Error difference of f(x)')
@@ -332,7 +334,7 @@ plt.grid(True)
 plt.savefig('pgfs/error_difference_bfgs.pgf', bbox_inches='tight')
 plt.show()
 
-plt.figure(figsize=(6, 3.6))
+plt.figure(figsize=(6, 3))
 plt.plot(np.abs(optimized_differences_bisection), 'o', label='Optimized difference of Bisection')
 plt.plot(np.abs(optimized_differences_bisection), 'r', label='Bisection method')
 plt.xlabel('Number of points')
@@ -343,7 +345,7 @@ plt.grid(True)
 plt.savefig('pgfs/optimized_differences_bisection.pgf', bbox_inches='tight')
 plt.show()
 
-plt.figure(figsize=(6, 3.6))
+plt.figure(figsize=(6, 3))
 plt.plot(np.abs(optimized_differences_bfgs), 's', label='Optimized difference of BFGS')
 plt.plot(np.abs(optimized_differences_bfgs), 'y', label='BFGS method')
 plt.xlabel('Number of points')
@@ -354,7 +356,7 @@ plt.grid(True)
 plt.savefig('pgfs/optimized_differences_bfgs.pgf', bbox_inches='tight')
 plt.show()
 
-plt.figure(figsize=(6, 3.6))
+plt.figure(figsize=(6, 3))
 plt.plot(np.abs(optimized_differences_bisection), 'o', label='Bisection method')
 plt.plot(np.abs(optimized_differences_bisection), 'r', label='Bisection method')
 plt.plot(np.abs(optimized_differences_bfgs), 's', label='BFGS method')
